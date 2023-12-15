96473a9b2579f8318e83b8976d71648d6cb64f73
Jowllel <joel.schaller16@gmail.com>
2023-12-15 16:22:33 +0100

    SW: CV_FW: Replaced Tabs with spaces

--- 6_Software/CV_FW/CM7/Core/Usercode/cvlib.h
+++ 6_Software/CV_FW/CM7/Core/Usercode/cvlib.h
@@ -7,14 +7,14 @@
 
 typedef enum
 {
-	CV_VRange_1__4_9V = 1,
-	CV_VRange_2__10_35V = 2
+    CV_VRange_1__4_9V = 1,
+    CV_VRange_2__10_35V = 2
 } CV_VRange_TypeDef;
 
 typedef enum
 {
-	CV_ARange_1__0_330A = 1,
-	CV_ARange_2__1_32A = 2
+    CV_ARange_1__0_330A = 1,
+    CV_ARange_2__1_32A = 2
 } CV_ARange_TypeDef;
 
 
